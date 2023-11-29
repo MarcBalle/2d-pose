f3f3dc0a27298ae84f0da48f57a1303c61aee56c
MarcBalle <balle.marc97@gmail.com>
2023-11-29 18:47:52 +0100

    add inference model and save results in video

--- detect_bounding_boxes.py
+++ detect_bounding_boxes.py
@@ -1,12 +1,19 @@
+import argparse
+import os
+
 import cv2
 from tqdm import tqdm
-import argparse
+
+from mmdet.apis import DetInferencer
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--video', '-v', type=str, required=True, help='path to the input video')
-    parser.add_argument('--frames', '-f', type=str, default='', help='number of frames to analyze')
+    parser.add_argument('--video', '-v', type=str, required=True, help='Path to the input video')
+    parser.add_argument('--frames', '-f', type=str, default='', help='Number of frames to analyze')
+    parser.add_argument('--config', '-c', type=str, required=True, help='mmdetection model config file')
+    parser.add_argument('--checkpoint', '-ckpt', type=str, required=True, help='pth checkpoint file')
+    parser.add_argument('--output', '-o', type=str, default='outputs', help='Output directory')
 
     args = parser.parse_args()
 
@@ -14,11 +21,37 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
+
+    if not os.path.isdir(args.output):
+        os.mkdir(args.output)
+
+    # Initialize the DetInferencer
+    inferencer = DetInferencer(model='yolox_x_8x8_300e_coco')
     
     cap = cv2.VideoCapture(args.video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-    video_length =  int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_length = int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    for i in tqdm(range(video_length)):
+     # Read first frame and perform inference
+    _, img = cap.read()
+    results = inferencer(img, return_vis=True)
+    bb_img = results['visualization'][0]
+    
+    h, w = img.shape[:2]
+    size = (w, h)
+    out = cv2.VideoWriter('bb_output.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+    out.write(bb_img)
+
+    for i in tqdm(range(1, video_length)):
         # Image read in BGR order (required by mmdetections)
         _, img = cap.read()
+
+        # Perform inference
+        results = inferencer(img, return_vis=True)
+        bb_img = results['visualization'][0]
+        out.write(bb_img)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
