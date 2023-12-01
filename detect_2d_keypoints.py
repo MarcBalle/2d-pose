3d1b0146716067177957891d1c3654a9ee19096d
MarcBalle <balle.marc97@gmail.com>
2023-12-01 18:10:48 +0100

    black

--- detect_2d_keypoints.py
+++ detect_2d_keypoints.py
@@ -7,45 +7,75 @@ import torch
 
 from mmpose.apis import MMPoseInferencer
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--video', '-v', type=str, required=True, help='Path to the input video')
-    parser.add_argument('--frames', '-f', type=str, default='', help='Number of frames to analyze')
-    parser.add_argument('--config_pose', '-cp', type=str, required=True, help='mmpose model config file')
-    parser.add_argument('--config_det', '-cd', type=str, required=True, help='mmdetection model config file')
-    parser.add_argument('--checkpoint', '-ckpt', type=str, required=False, help='pth checkpoint file')
-    parser.add_argument('--output', '-o', type=str, default='.', help='Output directory')
+    parser.add_argument(
+        "--video", "-v", type=str, required=True, help="Path to the input video"
+    )
+    parser.add_argument(
+        "--frames", "-f", type=str, default=None, help="Number of frames to analyze"
+    )
+    parser.add_argument(
+        "--config_pose", "-cp", type=str, required=True, help="mmpose model config file"
+    )
+    parser.add_argument(
+        "--config_det",
+        "-cd",
+        type=str,
+        required=True,
+        help="mmdetection model config file",
+    )
+    parser.add_argument(
+        "--checkpoint", "-ckpt", type=str, required=False, help="pth checkpoint file"
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, default=".", help="Output directory"
+    )
 
     args = parser.parse_args()
 
     return args
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_arguments()
 
-    if not os.path.isdir(os.path.join(args.output, 'output')):
-        os.mkdir(os.path.join(args.output, 'output'))
+    if not os.path.isdir(os.path.join(args.output, "output")):
+        os.mkdir(os.path.join(args.output, "output"))
 
     # Initialize the DetInferencer
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    inferencer = MMPoseInferencer(pose2d=args.config_pose, det_model=args.config_det, det_cat_ids=[0], device=device)  # the category id of 'human' class is [0]
-    
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    inferencer = MMPoseInferencer(
+        pose2d=args.config_pose,
+        det_model=args.config_det,
+        det_cat_ids=None if args.config_det == "whole_image" else [0],
+        device=device,
+    )
+
     cap = cv2.VideoCapture(args.video)
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    video_length = int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_length = (
+        int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    )
 
-     # Read first frame and perform inference
+    # Read first frame and perform inference
     _, img = cap.read()
     result_generator = inferencer(img, return_vis=True)
     results = next(result_generator)
-    bb_img = results['visualization'][0]
-    
+    bb_img = results["visualization"][0]
+
     h, w = img.shape[:2]
     size = (w, h)
     video_name, video_ext = os.path.splitext(os.path.split(args.video)[1])
-    out = cv2.VideoWriter(os.path.join(args.output, video_name + '.2dkeypoints' + video_ext), cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+    out = cv2.VideoWriter(
+        os.path.join(args.output, video_name + ".2dkeypoints" + video_ext),
+        cv2.VideoWriter_fourcc(*"DIVX"),
+        fps,
+        size,
+    )
     out.write(bb_img)
 
     for i in tqdm(range(1, video_length)):
@@ -55,7 +85,7 @@ if __name__ == '__main__':
         # Perform inference
         result_generator = inferencer(img, return_vis=True)
         results = next(result_generator)
-        bb_img = results['visualization'][0]
+        bb_img = results["visualization"][0]
         out.write(bb_img)
 
     cap.release()
--- detect_bounding_boxes.py
+++ detect_bounding_boxes.py
@@ -7,42 +7,61 @@ import torch
 
 from mmdet.apis import DetInferencer
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--video', '-v', type=str, required=True, help='Path to the input video')
-    parser.add_argument('--frames', '-f', type=str, default='', help='Number of frames to analyze')
-    parser.add_argument('--config', '-c', type=str, required=True, help='mmdetection model config file')
-    parser.add_argument('--checkpoint', '-ckpt', type=str, required=False, help='pth checkpoint file')
-    parser.add_argument('--output', '-o', type=str, default='.', help='Output directory')
+    parser.add_argument(
+        "--video", "-v", type=str, required=True, help="Path to the input video"
+    )
+    parser.add_argument(
+        "--frames", "-f", type=str, default="", help="Number of frames to analyze"
+    )
+    parser.add_argument(
+        "--config", "-c", type=str, required=True, help="mmdetection model config file"
+    )
+    parser.add_argument(
+        "--checkpoint", "-ckpt", type=str, required=False, help="pth checkpoint file"
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, default=".", help="Output directory"
+    )
 
     args = parser.parse_args()
 
     return args
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_arguments()
 
-    if not os.path.isdir(os.path.join(args.output, 'output')):
-        os.mkdir(os.path.join(args.output, 'output'))
+    if not os.path.isdir(os.path.join(args.output, "output")):
+        os.mkdir(os.path.join(args.output, "output"))
 
     # Initialize the DetInferencer
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     inferencer = DetInferencer(model=args.config, device=device)
-    
+
     cap = cv2.VideoCapture(args.video)
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    video_length = int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_length = (
+        int(args.frames) if args.frames else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    )
 
-     # Read first frame and perform inference
+    # Read first frame and perform inference
     _, img = cap.read()
     results = inferencer(img, return_vis=True)
-    bb_img = results['visualization'][0]
-    
+    bb_img = results["visualization"][0]
+
     h, w = img.shape[:2]
     size = (w, h)
-    out = cv2.VideoWriter(os.path.join(args.output, 'bb_output.avi'), cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+    out = cv2.VideoWriter(
+        os.path.join(args.output, "bb_output.avi"),
+        cv2.VideoWriter_fourcc(*"DIVX"),
+        fps,
+        size,
+    )
     out.write(bb_img)
 
     for i in tqdm(range(1, video_length)):
@@ -51,7 +70,7 @@ if __name__ == '__main__':
 
         # Perform inference
         results = inferencer(img, return_vis=True)
-        bb_img = results['visualization'][0]
+        bb_img = results["visualization"][0]
         out.write(bb_img)
 
     cap.release()
