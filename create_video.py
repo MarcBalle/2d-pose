15edda940269cdff15a9416f54bbeca39f02137f
MarcBalle <balle.marc97@gmail.com>
2024-03-07 18:26:15 +0100

    script for creating a video with keypoints

---
+++ create_video.py
@@ -0,0 +1,46 @@
+"""Script for creating a video with a given 2D skeleton"""
+
+import argparse
+import os
+
+import numpy as np
+import cv2
+
+from utils import show2Dpose
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video", type=str, required=True, help="video file path")
+    parser.add_argument("--kpts", type=str, required=True, help="keypoints file path")
+
+    args = parser.parse_args()
+
+    keypoints = np.load(args.kpts)["keypoints"]
+
+    cap = cv2.VideoCapture(args.video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH) 
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  
+
+    assert keypoints.shape[0] == video_length, f"Keypoints frames ({keypoints.shpe[0]}) and video frame ({video_length}) do not match."
+
+    output_dir = os.path.dirname(args.video)
+    video_name, video_ext = os.path.splitext(os.path.basename(args.video))
+    out = cv2.VideoWriter(
+        filename=os.path.join(output_dir, video_name + ".2dkeypoints" + video_ext),
+        fourcc=cv2.VideoWriter_fourcc(*"DIVX"),
+        fps=fps,
+        frameSize=(int(width), int(height)),    
+    )
+
+    for i in range(video_length):
+        _, img = cap.read()
+        keypoints_frame = keypoints[i]
+        skeleton_image = show2Dpose(keypoints_frame, img)
+        out.write(skeleton_image)
+    
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
